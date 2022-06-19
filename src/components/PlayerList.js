6fc472a98c2c13e1550152ff39fafc52cd3a1cbc
shamique202 <shamique.e@gmail.com>
2022-06-19 01:30:31 -0400

    updated react context and imported w/consumer

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Consumer } from './Consumer';
+import { Consumer } from './Context';
 import Player from './Player';
 
 const PlayerList = (props) => {
   return (
     <Consumer>
       { context => (
-        <React.Fragment>
-      {context.map( (player, index) =>
-        <Player 
-          {...player}
-          key={player.id.toString()} 
-          index={index}
-          changeScore={props.changeScore}
-          removePlayer={props.removePlayer}           
-        />
-      )}
-    </React.Fragment>
+            <React.Fragment>
+          {context.map( (player, index) =>
+            <Player 
+              {...player}
+              key={player.id.toString()} 
+              index={index}
+              changeScore={props.changeScore}
+              removePlayer={props.removePlayer}           
+            />
+          )}
+              </React.Fragment>
       )}
     </Consumer>
-   
   );
 }
 
